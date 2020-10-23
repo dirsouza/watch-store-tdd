fd2fe124515ef91b02177f6d0bd6f085784980ff
Diogo Souza <diogo.souza@uds.com.br>
2020-10-22 23:49:39 -0400

    pacotes, componentes e configurações

---
+++ miragejs/server.js
@@ -0,0 +1,17 @@
+import { Server } from 'miragejs';
+import factories from './factories';
+import routes from './routes';
+import models from './models';
+import seeds from './seeds';
+
+const config = (environment) => ({
+  environment,
+  factories,
+  models,
+  routes,
+  seeds,
+});
+
+export function makeServer({ environment = 'development' } = {}) {
+  return new Server(config(environment));
+}
